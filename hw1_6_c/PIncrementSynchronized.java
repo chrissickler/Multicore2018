e4291676ac0e2e791adfa36e32b70d3e283d007c
chrissickler <christopher.sickler@utexas.edu>
2018-09-18 16:54:47 -0500

    Reentrant and Synch are updated with Runnable

--- hw1_6_c/PIncrementSynchronized.java
+++ hw1_6_c/PIncrementSynchronized.java
@@ -1,20 +1,25 @@
 
 
-public class PIncrementSynchronized {
+public class PIncrementSynchronized implements Runnable{
 
 	static volatile int num;
+	int times;
+
+	public PIncrementSynchronized(int times) {
+		this.times=times;
+	}
 
 	public static int parallelIncrement(int c, int numThreads) {
-		MyThread t[] = new MyThread[numThreads];
+		PIncrementSynchronized t[] = new PIncrementSynchronized[numThreads];
 		num = c;
 		int mod = 1200000 % numThreads;
 		for(int i =0;i<numThreads;i++) {
 			if(mod!=0) {
-				t[i] = new MyThread(1200000/numThreads + 1);
+				t[i] = new PIncrementSynchronized(1200000/numThreads + 1);
 				mod--;
 			}
 			else
-				t[i] = new MyThread(1200000/numThreads);
+				t[i] = new PIncrementSynchronized(1200000/numThreads);
 		}
 		for(int i=0;i<t.length;i++) {
 			t[i].run();
@@ -24,25 +29,37 @@ public class PIncrementSynchronized {
 		return num;
 	}
 
-	static class MyThread extends Thread {
-		int times;
-		Object obj = new Object();
-		public MyThread(int times) {
-
-			this.times = times;
-		}
-		public void run() {
-			while(times!=0){
-				synchronized(this) {
-					num++;
-				}
-				times--;
-
+	@Override
+	public void run() {
+		while(times!=0) {
+			synchronized(this) {
+				num++;
 			}
+			this.times--;
 		}
-
-
 	}
+
+//	static class MyThread extends Thread {
+//		int times;
+//		Object obj = new Object();
+//		public MyThread(int times) {
+//
+//			this.times = times;
+//		}
+//		public void run() {
+//			while(times!=0){
+//				synchronized(this) {
+//					num++;
+//				}
+//				times--;
+//
+//			}
+//		}
+//
+//
+//	}
+
+
 
 }
 
--- hw1_6_d/PIncrementReentrant.java
+++ hw1_6_d/PIncrementReentrant.java
@@ -1,20 +1,27 @@
 import java.util.concurrent.locks.ReentrantLock;
 
-public class PIncrementReentrant {
+public class PIncrementReentrant implements Runnable{
 	static volatile int num;
 	static ReentrantLock re;
+	int times;
+
+	public PIncrementReentrant(int times) {
+		this.times = times;
+		re = new ReentrantLock();
+	}
+
 
 	public static int parallelIncrement(int c, int numThreads) {
-		MyThread t[] = new MyThread[numThreads];
+		PIncrementReentrant t[] = new PIncrementReentrant[numThreads];
 		num = c;
 		int mod = 1200000 % numThreads;
 		for(int i =0;i<numThreads;i++) {
 			if(mod!=0) {
-				t[i] = new MyThread(1200000/numThreads + 1);
+				t[i] = new PIncrementReentrant(1200000/numThreads + 1);
 				mod--;
 			}
 			else
-				t[i] = new MyThread(1200000/numThreads);
+				t[i] = new PIncrementReentrant(1200000/numThreads);
 		}
 		for(int i=0;i<t.length;i++) {
 			t[i].run();
@@ -24,25 +31,30 @@ public class PIncrementReentrant {
 		return num;
 	}
 
-	static class MyThread extends Thread {
-		int times;
-		Object obj = new Object();
-		public MyThread(int times) {
-			re = new ReentrantLock();
-			this.times = times;
-		}
-		public void run() {
-			while(times!=0){
-				boolean isLocked = re.tryLock();
-				if(isLocked) {
-					re.lock();
-					num++;
-					times--;
-					re.unlock();
-				}
-
-
+	@Override
+	public void run() {
+		while(times!=0){
+			boolean isLocked = re.tryLock();
+			if(isLocked) {
+				re.lock();
+				num++;
+				times--;
+				re.unlock();
 			}
 		}
 	}
+
+//	static class MyThread extends Thread {
+//		int times;
+//		Object obj = new Object();
+//		public MyThread(int times) {
+//			re = new ReentrantLock();
+//			this.times = times;
+//		}
+//		public void run() {
+//
+//		}
+//	}
+
+
 }
